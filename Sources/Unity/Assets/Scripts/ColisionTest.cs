b3d1bc08899c62bc0621560b7aa1c9a600fc0a6d
soetshi <90394673+soetshi@users.noreply.github.com>
2022-11-19 23:21:15 +0100

    Ajout de mes assets + scene

---
+++ Sources/Unity/Assets/Scripts/ColisionTest.cs
@@ -0,0 +1,20 @@
+
+
+using UnityEngine;
+
+public class ColisionTest : MonoBehaviour
+{
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+         if(collision.gameObject.CompareTag("Obstacle")){
+
+             Debug.Log("Collision with " + collision.gameObject.tag);
+
+         }
+
+
+    }
+}
+ 
+   
