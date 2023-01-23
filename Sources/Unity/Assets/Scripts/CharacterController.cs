a8431215c54e25f7e9ce903706cbb03fb40b96e3
soetshi <90394673+soetshi@users.noreply.github.com>
2023-01-23 21:17:01 +0100

    Ajout des bonus Nitro, Super Nitro, gravity

--- Sources/Unity/Assets/Scripts/CharacterController.cs
+++ Sources/Unity/Assets/Scripts/CharacterController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
@@ -8,11 +10,15 @@ public class CharacterController : MonoBehaviour
 	public Transform feetTransform;
 	private Vector3 movementIntent;
 	public Rigidbody rb;
-
+	
     void Start()
     {
-        
+
+	
+
     }
+
+   
     
     void Update()
     {
@@ -49,7 +55,7 @@ public class CharacterController : MonoBehaviour
 	movementIntent = movementIntent.normalized;
 	}
 
-	private void FixedUpdate()
+    private void FixedUpdate()
     {
 		//teste pr eculer + lent que avancer
 		/*if (Input.GetKey(KeyCode.S))
@@ -65,4 +71,45 @@ public class CharacterController : MonoBehaviour
 		}*/
         rb.AddForce(movementForce * (feetTransform.rotation * movementIntent), ForceMode.Acceleration);
     }
+    
+    
+   public IEnumerator Nitro(float count = 5f)
+    {
+
+	    movementForce *= 2f;
+
+	    yield return new WaitForSeconds(count);
+	    
+	    movementForce *= 0.5f;
+
+    }
+    
+    public IEnumerator SuperNitro(float count = 10f)
+    {
+	    
+	    movementForce *= 2f;
+		
+	    yield return new WaitForSeconds(count);
+
+	    movementForce *= 0.5f;
+
+
+    }
+    
+    public IEnumerator Gravity(float count = 5f)
+    {
+	    movementForce *= 0.5f;
+	    
+	    yield return new WaitForSeconds(count);
+	    
+	    movementForce *= 2f;
+	   
+    }
+
+
+    
+    
+    
+
+
 }
