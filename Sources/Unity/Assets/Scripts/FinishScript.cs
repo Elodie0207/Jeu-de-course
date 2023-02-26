7af2c81271c25a1bc4ceb51c86fcad75be206680
Elodie0207 <elodieguldas@gmail.com>
2023-02-26 19:00:34 +0100

    Elodie

--- Sources/Unity/Assets/Scripts/FinishScript.cs
+++ Sources/Unity/Assets/Scripts/FinishScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+<<<<<<< Updated upstream
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
@@ -32,6 +33,35 @@ public class FinishScript : MonoBehaviour
             {
                 nbTourRestant++;
                 tours.text = nbTourRestant + "/" + nbTour;
+=======
+
+using UnityEngine.UI;
+public class FinishScript : MonoBehaviour
+{
+    public GameObject tour;
+        
+    void OnTriggerEnter(Collider collide){
+     
+        if(collide.tag=="Finish")
+        {
+            if (tour.GetComponent<Text>().text == "3")
+            {
+                Debug.Log("Finish");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+            }
+            else if(tour.GetComponent<Text>().text == "0")
+            {
+                tour.GetComponent<Text>().text = "1";
+            }
+            else if(tour.GetComponent<Text>().text == "1")
+            {
+                tour.GetComponent<Text>().text = "2";
+            }
+            else if(tour.GetComponent<Text>().text == "2")
+            {
+                tour.GetComponent<Text>().text = "3";
+>>>>>>> Stashed changes
             }
         }
     }
