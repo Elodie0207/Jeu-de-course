35f14f18870435375946b90ce5071457b3edf94c
Elodie0207 <elodieguldas@gmail.com>
2023-04-11 23:23:28 +0200

    ResolutionMenu (Elodie)

--- Sources/Unity/Assets/Scripts/Menu.cs
+++ Sources/Unity/Assets/Scripts/Menu.cs
@@ -9,12 +9,27 @@ public class Menu : MonoBehaviour
 public GameObject boutton_jouer;
 public GameObject boutton_quitter;
 private string langage="";
-public GameObject boutton;
+//public GameObject boutton;
 public int test = 0;
+public GameObject CanvaCourrant;
+public GameObject nouveauCanva;
+public Dropdown drop;
+public Button boutton_retour;
 public void Start()
 {
+	 CanvaCourrant.SetActive(true); 
+	nouveauCanva.SetActive(false);
 	Screen.fullScreen = true; 
 	buttonResol.gameObject.SetActive(false); 
+drop.options.Clear();
+        drop.options.Add(new Dropdown.OptionData("Fullscreen"));
+        drop.options.Add(new Dropdown.OptionData("Fenêtre"));
+        drop.options.Add(new Dropdown.OptionData("1920x1080"));
+        drop.options.Add(new Dropdown.OptionData("2560x1440"));
+		drop.onValueChanged.AddListener(delegate
+        {
+            OnScreenModeChanged(drop.value);
+        });
 }
     // Start is called before the first frame update
     public void Jouer(){
@@ -27,7 +42,8 @@ public void Start()
     }
     public void Param(){
 	    buttonResol.gameObject.SetActive(true); 
-         
+        CanvaCourrant.SetActive(false); 
+		nouveauCanva.SetActive(true); 
     }
 	public void Langage(){
 		
@@ -48,22 +64,31 @@ public void Start()
 			
 	
 }
-
-	public void fullScreen()
-	{
-		if (Screen.fullScreen)
-		{
-			Screen.fullScreen = false;
-
-			Screen.SetResolution(1280, 720, false);
-			buttonResol.gameObject.SetActive(false); 
-		}
-		else
-		{
-			Screen.fullScreen = true; 
-			buttonResol.gameObject.SetActive(false); 
-		}
-	}
+public void OnScreenModeChanged(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                Screen.fullScreen = true;
+                break;
+            case 1:
+                Screen.fullScreen = false;
+                break;
+            case 2:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 3:
+                Screen.SetResolution(2560, 1440, Screen.fullScreen);
+                break;
+            default:
+                Debug.LogError("Erreur");
+                break;
+        }
+    }
+public void Retour(){
+ 	CanvaCourrant.SetActive(true); 
+		nouveauCanva.SetActive(false); 
+}
 	}
 
 
