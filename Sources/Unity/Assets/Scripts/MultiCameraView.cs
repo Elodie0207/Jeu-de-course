9b3f92d9273f4eb6af6905595fece6cee4c0ef1a
soetshi <90394673+soetshi@users.noreply.github.com>
2023-04-15 20:19:00 +0200

    Ajout de la manipulation de caméra pour le deuxième joueur

---
+++ Sources/Unity/Assets/Scripts/MultiCameraView.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour
+{
+    
+    public Camera CamFirstPerson;
+    public Camera CamThirdPerson;
+	public Camera CamBack;
+    public bool CamEtat;
+
+    public void ShowThirdPersonView() {
+        CamFirstPerson.enabled = false;
+        CamThirdPerson.enabled = true;
+        CamEtat = false;
+    }
+    public void ShowFirstPersonView() {
+        CamFirstPerson.enabled = true;
+        CamThirdPerson.enabled = false;
+        CamEtat = true;
+    }
+
+	public void ShowBackView() {
+        CamBack.enabled = true;
+		CamFirstPerson.enabled = false;
+        CamThirdPerson.enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowThirdPersonView();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+		if (Input.GetKey(KeyCode.U))
+		{
+			ShowBackView();
+        }else {
+            if (CamEtat == false)
+            {
+                ShowThirdPersonView();
+            }else
+            {
+                ShowFirstPersonView();
+            }
+		}
+
+		if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (CamEtat == true)
+            {
+                ShowThirdPersonView();
+            }else
+            {
+                ShowFirstPersonView();
+            }
+        }
+	}
+}
