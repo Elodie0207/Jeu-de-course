e45b64df2334e70baae3b4cae90a85a84e90a4e7
Elodie0207 <elodieguldas@gmail.com>
2023-04-27 22:56:03 +0200

    ChoixMap

--- Sources/Unity/Assets/Scripts/ChoixMap.cs
+++ Sources/Unity/Assets/Scripts/ChoixMap.cs
@@ -15,19 +15,13 @@ public class ChoixMap : MonoBehaviour
     {
         string map = PlayerPrefs.GetString("map");
         Debug.Log(map);
-        
-<<<<<<< Updated upstream
 
-        Piste1.SetActive(false); 
-        Piste2.SetActive(true);
-=======
         Piste1.SetActive(true); 
         Piste2.SetActive(false);
->>>>>>> Stashed changes
         Piste3.SetActive(false);
         Piste4.SetActive(false);
 
-      /*if (map == "Map1")
+      if (map == "Map1")
 
         {
             Piste1.SetActive(true); 
