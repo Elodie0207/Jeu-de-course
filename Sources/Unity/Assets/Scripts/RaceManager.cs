97df95b243f46a905a83406b63ecc20c6e3c4d7e
soetshi <90394673+soetshi@users.noreply.github.com>
2023-07-06 12:06:16 +0200

    Mis à jour du Scoreboard entre les courses

--- Sources/Unity/Assets/Scripts/RaceManager.cs
+++ Sources/Unity/Assets/Scripts/RaceManager.cs
@@ -5,65 +5,61 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RaceManager : MonoBehaviour {
-
-    // List des FinishScript pour chaque coureur
+public class RaceManager : MonoBehaviour 
+{
     public List<FinishScript> racers;
-
-    // List pour stocker les positions des coureurs
     public List<int> positions;
     
     public Text[] nameTexts;
     public Text[] positionTexts;
     public Text[] scoreTexts;
     
-    void Start() {
-        
-        // Désactive les FinishScripts et PlayerUI des joueurs inactifs
-        foreach (FinishScript racer in racers) {
-            if (!racer.gameObject.activeSelf) {
+    void Start() 
+    {
+        foreach (FinishScript racer in racers) 
+        {
+            if (!racer.gameObject.activeSelf) 
+            {
                 racer.enabled = false;
             }
         }
         
-        // Enlève les FinishScript des joueurs désactivés de la liste "racers"
         racers.RemoveAll(racer => !racer.gameObject.activeSelf);
     }
 
-    void Update () {
-        
-        // Appelle la fonction de mise à jour des positions des coureurs
+    void Update () 
+    {
         UpdateRacerPositions();
-        
     }
 
-    // Fonction de mise à jour des positions des coureurs
-    private void UpdateRacerPositions() {
-       // Debug.Log("UpdateRacerPositions() called.");
+    private void UpdateRacerPositions() 
+    {
         positions.Clear();
-        foreach (FinishScript racer in racers) {
+        foreach (FinishScript racer in racers) 
+        {
             int position = 1;
-            foreach (FinishScript otherRacer in racers) {
-                if (otherRacer.nbTours > racer.nbTours) {
+            foreach (FinishScript otherRacer in racers) 
+            {
+                if (otherRacer.nbTours > racer.nbTours) 
+                {
                     position++;
-                } else if (otherRacer.nbTours == racer.nbTours && otherRacer.GetComponent<TrackPosition>().object1Position.z > racer.GetComponent<TrackPosition>().object1Position.z) {
+                } 
+                else if (otherRacer.nbTours == racer.nbTours && otherRacer.GetComponent<TrackPosition>().object1Position.z > racer.GetComponent<TrackPosition>().object1Position.z) 
+                {
                     position++;
                 }
             }
             positions.Add(position);
-           // Debug.Log(racer.name + " est en " + position + "ème position.");
         }
 
-        // Met à jour la variable "positions" en temps réel
-        for (int i = 0; i < positions.Count; i++) {
+        for (int i = 0; i < positions.Count; i++) 
+        {
             positions[i] = positions.Count(p => p < positions[i]) + 1;
         }
-
     }
 
     public void UpdateScore()
     {   
-        // Trie les joueurs en fonction de leur position
         FinishScript[] sortedRacers = racers.OrderBy(r => positions[racers.IndexOf(r)]).ToArray();
 
         for (int i = 0; i < sortedRacers.Length; i++)
@@ -71,47 +67,28 @@ public class RaceManager : MonoBehaviour {
             FinishScript racer = sortedRacers[i];
             int racerIndex = racers.IndexOf(racer);
 
-            int score = 0;
-
             switch (positions[racerIndex])
             {
                 case 1:
-                    score = 10;
+                    ScoreManager.Instance.UpdateScore(racer.name, 10);
                     break;
                 case 2:
-                    score = 7;
+                    ScoreManager.Instance.UpdateScore(racer.name, 7);
                     break;
                 case 3:
-                    score = 5;
+                    ScoreManager.Instance.UpdateScore(racer.name, 5);
                     break;
                 case 4:
-                    score = 3;
+                    ScoreManager.Instance.UpdateScore(racer.name, 3);
                     break;
                 case 5:
-                    score = 2;
+                    ScoreManager.Instance.UpdateScore(racer.name, 2);
                     break;
             }
 
-            racer.score += score;
-
             nameTexts[i].text = racer.name;
             positionTexts[i].text = positions[racerIndex].ToString();
-            scoreTexts[i].text = racer.score.ToString();
+            scoreTexts[i].text = ScoreManager.Instance.GetScore(racer.name).ToString();
         }
-
-
-
-    }
-    
-    public int getRacerPosition(FinishScript racer)
-    {
-        // Trie la liste des joueurs en fonction de leur score
-        List<FinishScript> sortedRacers = racers.OrderByDescending(r => r.score).ToList();
-
-        // Trouve l'index du joueur dans la liste triée
-        int position = sortedRacers.IndexOf(racer);
-
-        // Ajoute 1 pour obtenir la position du joueur dans la course
-        return position + 1;
     }
 }
---
+++ Sources/Unity/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    // Dictionary to hold player scores. Key is the player's name.
+    private Dictionary<string, int> playerScores = new Dictionary<string, int>();
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        // Don't destroy this object when a new scene is loaded
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void UpdateScore(string playerName, int score)
+    {
+        // If the player's name doesn't exist in the dictionary,
+        // add it with the score.
+        if (!playerScores.ContainsKey(playerName))
+        {
+            playerScores[playerName] = score;
+        }
+        // If the player's name is already in the dictionary, add the
+        // new score to the existing one.
+        else
+        {
+            playerScores[playerName] += score;
+        }
+    }
+
+    // Optional: method to get a player's score. Returns 0 if the player's name doesn't exist in the dictionary.
+    public int GetScore(string playerName)
+    {
+        if (playerScores.ContainsKey(playerName))
+        {
+            return playerScores[playerName];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    // Optional: method to get all scores.
+    public Dictionary<string, int> GetAllScores()
+    {
+        return playerScores;
+    }
+}
