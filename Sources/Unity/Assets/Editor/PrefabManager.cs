cf38c8f464e2959bab151403bfe034ad77e4d3be
soetshi <90394673+soetshi@users.noreply.github.com>
2023-07-09 13:46:49 +0200

    ajout image + update PrefabManager

--- Sources/Unity/Assets/Editor/PrefabManager.cs
+++ Sources/Unity/Assets/Editor/PrefabManager.cs
@@ -5,6 +5,7 @@ public class PrefabManager : EditorWindow
 {
     GameObject prefab;
     GameObject course; // pour sélectionner la course
+    bool isPlacingPrefab = false; // pour contrôler si on place des prefabs ou non
 
     [MenuItem("Tools/Prefab Placer")]
     static void OpenWindow()
@@ -18,7 +19,7 @@ public class PrefabManager : EditorWindow
         course = (GameObject)EditorGUILayout.ObjectField("Course:", course, typeof(GameObject), true);
         prefab = (GameObject)EditorGUILayout.ObjectField("Prefab:", prefab, typeof(GameObject), false);
 
-        if (GUILayout.Button("Place Prefab"))
+        if (GUILayout.Button("Start Placing Prefabs"))
         {
             if(prefab == null)
             {
@@ -31,21 +32,32 @@ public class PrefabManager : EditorWindow
                 Debug.LogError("No course selected");
                 return;
             }
+            
+            isPlacingPrefab = true;
+            SceneView.duringSceneGui += OnSceneGUI;
+        }
 
-            SceneView.duringSceneGui += view =>
+        if (GUILayout.Button("Stop Placing Prefabs"))
+        {
+            isPlacingPrefab = false;
+            SceneView.duringSceneGui -= OnSceneGUI;
+        }
+    }
+
+    private void OnSceneGUI(SceneView view)
+    {
+        if (!isPlacingPrefab) return;
+
+        Event e = Event.current;
+        if (e.type == EventType.MouseDown && e.button == 0)
+        {
+            Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
             {
-                Event e = Event.current;
-                if (e.type == EventType.MouseDown && e.button == 0)
-                {
-                    Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit))
-                    {
-                        GameObject instance = Instantiate(prefab, hit.point, Quaternion.identity);
-                        instance.transform.SetParent(course.transform); // ajoute le prefab comme un enfant de la course
-                    }
-                }
-            };
+                GameObject instance = Instantiate(prefab, hit.point, Quaternion.identity);
+                instance.transform.SetParent(course.transform); // ajoute le prefab comme un enfant de la course
+            }
         }
     }
 }
